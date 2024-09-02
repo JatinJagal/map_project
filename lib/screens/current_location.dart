600ab87110d1ab799732053e5d057df3bb7f2ccd
JATIN JAGAL <jateenjagal219@gmail.com>
2024-09-02 15:08:29 +0530

    Changes

--- lib/screens/current_location.dart
+++ lib/screens/current_location.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class CurrentLocationScreen extends StatefulWidget {
   const CurrentLocationScreen({Key? key}) : super(key: key);
 
@@ -9,72 +11,135 @@ class CurrentLocationScreen extends StatefulWidget {
 }
 
 class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
+  MapType type = MapType.normal;
 
   late GoogleMapController googleMapController;
-  static const CameraPosition initialCameraPotion = CameraPosition(target: LatLng(37.42796133580664, -122.085749655962),zoom: 14);
+  static const CameraPosition initialCameraPotion = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14);
 
   Set<Marker> marker = {};
+
+  //new
+  String? _address;
+  void _handleTap(LatLng tappedPoint) async {
+    setState(() {
+      marker = {};
+      marker.add(
+        Marker(
+          markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+          infoWindow: InfoWindow(
+            title: 'New Marker',
+            snippet: '${tappedPoint.latitude}, ${tappedPoint.longitude}',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+      );
+    });
+
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        tappedPoint.latitude,
+        tappedPoint.longitude,
+      );
+      if (placemarks.isNotEmpty) {
+        Placemark place = placemarks.first;
+        setState(() {
+          _address =
+              " ${place.street}"; //${place.street}, ${place.subLocality},
+        });
+        print(_address); // Print the address to console
+      }
+    } catch (e) {
+      print("Failed to get address: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Current Location'),
+        title: _address != null ? Text(_address!) : const Text("Location"),
         centerTitle: true,
       ),
-      body: GoogleMap(initialCameraPosition: initialCameraPotion,markers: marker,zoomControlsEnabled: false,
-        mapType: MapType.satellite,
-        onMapCreated: (GoogleMapController controller){
+      body: GoogleMap(
+        initialCameraPosition: initialCameraPotion,
+        markers: marker,
+        mapToolbarEnabled: false,
+        indoorViewEnabled: true,
+        zoomControlsEnabled: false,
+        mapType: type, //MapType.satellite
+        fortyFiveDegreeImageryEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
           googleMapController = controller;
-        },),
-      floatingActionButton: FloatingActionButton.extended(onPressed: ()async{
-        Position position = await _diterminePosition();
+        },
+        onTap: _handleTap,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  type = MapType.satellite;
+                });
+              },
+              child: const Text("Setellite")),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  type = MapType.normal;
+                });
+              },
+              child: const Text("Normal")),
+          FloatingActionButton.extended(
+              onPressed: () async {
+                Position position = await _diterminePosition();
 
-        googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(position.latitude, position.longitude),zoom: 14)));
+                googleMapController.animateCamera(
+                    CameraUpdate.newCameraPosition(CameraPosition(
+                        target: LatLng(position.latitude, position.longitude),
+                        zoom: 14)));
 
-        marker.clear();
-        
-        marker.add(Marker(markerId: MarkerId('current location'),position: LatLng(position.latitude, position.longitude)));
+                marker.clear();
 
-        setState(() {
-        });
-      },
-          label: Text('My Location')),
+                marker.add(Marker(
+                    markerId: const MarkerId('current location'),
+                    position: LatLng(position.latitude, position.longitude)));
+
+                setState(() {});
+              },
+              label: const Text('My Location')),
+        ],
+      ),
     );
   }
 
-  Future<Position> _diterminePosition() async{
-
+  Future<Position> _diterminePosition() async {
     bool serviceEnabled;
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
-    if(!serviceEnabled){
+    if (!serviceEnabled) {
       return Future.error('Location services are disable');
     }
 
     var permission = await Geolocator.checkPermission();
 
-    if(permission == LocationPermission.denied){
-
+    if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
 
-      if(permission == LocationPermission.denied){
-          return Future.error("Location permission denied");
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
       }
     }
 
-    if(permission == LocationPermission.deniedForever){
-
+    if (permission == LocationPermission.deniedForever) {
       return Future.error("Permently denied");
-
     }
 
     Position position = await Geolocator.getCurrentPosition();
 
     return position;
-
   }
-
 }
-
